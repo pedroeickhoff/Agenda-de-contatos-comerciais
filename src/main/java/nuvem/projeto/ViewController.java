4b0ddcd5fc9e23308d629b8f014bd7c0573df01f
Pedro Augusto <pedroaeickhoff@gmail.com>
2025-08-17 21:07:52 -0300

    Correção botão voltar

--- src/main/java/nuvem/projeto/ViewController.java
+++ src/main/java/nuvem/projeto/ViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ViewController {
 
-    @GetMapping("/home.html")
+    @GetMapping("/")
     public String index() {
         return "home"; 
     }
